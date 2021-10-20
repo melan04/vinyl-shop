c8977e0651e29011aa683e2f10881212a70a7492
Mélany Condé <melany.conde@gmail.com>
2021-10-20 20:40:09 +0200

    mise en place de l'app

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import '../styles/index.css';
+import Banner from './Banner'
+
+function App() {
+    return <Banner />
+}
+
+export default App
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import '../styles/Banner.css'
+
+function Banner() {
+    return <div className='vs-banner'>
+    <h1>Vinyl Shop</h1>
+</div>
+}
+
+export default Banner
